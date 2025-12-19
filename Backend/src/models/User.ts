8ac631d7879f0669ce2453f1977a427a0d62fa8e
robjor88 <robjor88@gmail.com>
2025-12-19 18:15:50 +0100

    bcrypt i userPassword

--- Backend/src/models/User.ts
+++ Backend/src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document } from "mongoose";
+import bcrypt from "bcrypt";
 
 export interface IUser extends Document {
   email: string;
@@ -8,6 +9,19 @@ export interface IUser extends Document {
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+});
+
+UserSchema.pre<IUser>("save", async function (next) {
+  const user = this;
+
+  if (!this.isModified("password")) return;
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  } catch (error: any) {
+    throw error;
+  }
 });
 
 export const User = mongoose.model<IUser>("User", UserSchema);
