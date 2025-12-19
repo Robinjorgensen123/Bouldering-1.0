29f70d7ed26bbe50f35ee47d7ee039e278bfe0ee
robjor88 <robjor88@gmail.com>
2025-12-19 18:03:13 +0100

    user test 2 done

--- Backend/src/__tests__/User.test.ts
+++ Backend/src/__tests__/User.test.ts
@@ -2,10 +2,17 @@ import { it, expect, describe } from "vitest";
 import { User } from "../models/User.js";
 
 describe("User Model Test", () => {
-  it("should create a user with an email, and a password", async () => {
+  it("should save the correct email", async () => {
     const user = new User({ email: "test@test.com", password: "password123" });
-
+    await user.save();
     expect(user.email).toBe("test@test.com");
-    expect(user.password).toBe("password123");
+  });
+  it("should never store passwords in plain text", async () => {
+    const password = "password123";
+    const user = new User({ email: "secure@test.com", password: password });
+    await user.save();
+
+    expect(user.password).not.toBe(password);
+    expect(user.password!.length).toBeGreaterThan(20);
   });
 });
