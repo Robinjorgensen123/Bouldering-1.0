a2a8f35a256a7d996f137dc20f8e319e57505b08
robjor88 <robjor88@gmail.com>
2025-12-19 12:28:27 +0100

    user test

---
+++ Backend/src/__tests__/setup.ts
@@ -0,0 +1,27 @@
+import { beforeAll, afterAll, afterEach } from "vitest";
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    if (collection) {
+      await collection.deleteMany({});
+    }
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
