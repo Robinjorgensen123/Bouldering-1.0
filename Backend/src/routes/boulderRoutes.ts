7b86065fe414a37450a8a04256df79896914ce37
robjor88 <robjor88@gmail.com>
2025-12-27 12:15:53 +0100

    joi shcmeas for boulders and user

--- Backend/src/routes/boulderRoutes.ts
+++ Backend/src/routes/boulderRoutes.ts
@@ -6,12 +6,14 @@ import {
   updateBoulder,
 } from "../controllers/boulderController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { validate } from "../middleware/validate.js";
+import { boulderSchema } from "../validators/boulderValidator.js";
 
 const router = Router();
 
-router.post("/", protect, createBoulder);
+router.post("/", protect, validate(boulderSchema), createBoulder);
 router.get("/", protect, getBoulders);
-router.put("/:id", protect, updateBoulder);
+router.put("/:id", protect, validate(boulderSchema), updateBoulder);
 router.delete("/:id", protect, deleteBoulder);
 
 export default router;
