d54e55295de755c2546e1e51f5f93f98d7856bfb
robjor88 <robjor88@gmail.com>
2025-12-27 13:21:19 +0100

    cloudinary img storage

---
+++ Backend/src/config/cloudinary.ts
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+  api_key: process.env.CLOUDINARY_API_KEY as string,
+  api_secret: process.env.CLOUDINARY_API_SECRET as string,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: "boulders",
+      allowed_formats: ["jpg", "png", "jpeg"],
+      transformation: [{ width: 1200, height: 1200, crop: "limit" }],
+    };
+  },
+});
+
+export const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
