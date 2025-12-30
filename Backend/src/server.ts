1cd7e7ea761771d8a6a3483287f4bddd7d821936
robjor88 <robjor88@gmail.com>
2025-12-30 20:04:59 +0100

    MongoDB Atlas setup and connect

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -3,8 +3,15 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import boulderRoutes from "./routes/boulderRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
+
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
@@ -14,6 +21,12 @@ app.use("/api/boulders", boulderRoutes);
 
 app.get("/health", (_req, res) => {
   res.status(200).json({ status: "ok" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
 export { app };
